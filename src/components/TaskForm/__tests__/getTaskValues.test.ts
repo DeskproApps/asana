55b3aa260b0565e49613414fdc00c34fdb6aad70
ilia makarov <ilia.makarov@me.com>
2023-06-09 15:48:16 +0300

    Wrote tests for Edit task

--- src/components/TaskForm/__tests__/getTaskValues.test.ts
+++ src/components/TaskForm/__tests__/getTaskValues.test.ts
@@ -1,8 +1,18 @@
 import { getTaskValues } from "../utils";
 
+const values = {
+  workspace: "1201873797723140",
+  project: "1204600346922820",
+  name: "Task from DP",
+  description: "this is description!!!",
+  status: "completed",
+  assignee: "1201873803770270",
+  dueDate: new Date("2023-06-30T21:00:00.000Z"),
+  tags: ['1204600348650521', '1204784700956466'],
+};
 
 describe("getTaskValues", () => {
-  test("should return required valuse", () => {
+  test("should return required values", () => {
     expect(getTaskValues({
       workspace:"w001",
       project:"p001",
@@ -17,16 +27,7 @@ describe("getTaskValues", () => {
   });
 
   test("should return full task values", () => {
-    expect(getTaskValues({
-      workspace:"1201873797723140",
-      project:"1204600346922820",
-      name:"Task from DP",
-      description:"this is description!!!",
-      status:"completed",
-      assignee:"1201873803770270",
-      dueDate: new Date("2023-06-30T21:00:00.000Z"),
-      tags: ['1204600348650521', '1204784700956466'],
-    })).toEqual({
+    expect(getTaskValues(values)).toEqual({
       workspace:"1201873797723140",
       projects:['1204600346922820'],
       name:"Task from DP",
@@ -35,6 +36,17 @@ describe("getTaskValues", () => {
       due_on:"2023-06-30",
       notes:"this is description!!!",
       tags: ['1204600348650521', '1204784700956466'],
+    });
+  });
+
+  test("should return date for update task", () => {
+    expect(getTaskValues(values, true)).toEqual({
+      workspace:"1201873797723140",
+      name:"Task from DP",
+      completed: true,
+      assignee:"1201873803770270",
+      due_on:"2023-06-30",
+      notes:"this is description!!!",
     });
   });
 });
--- src/components/ViewTask/blocks/__tests__/Details.test.tsx
+++ src/components/ViewTask/blocks/__tests__/Details.test.tsx
@@ -14,13 +14,13 @@ describe("ViewTask", () => {
         <Details task={mockTask.data as never} />
       ), { wrappers: { theme: true } });
 
-      expect(await findByText(/Link Asana tasks to ticket DP/i)).toBeInTheDocument();
+      expect(await findByText(/Mock Task/i)).toBeInTheDocument();
       expect(await findByText(/Project: Deskpro Asana App/i)).toBeInTheDocument();
-      expect(await findByText(/ilia makarov/i)).toBeInTheDocument();
+      expect(await findByText(/Armen Tamzarian/i)).toBeInTheDocument();
       expect(await findByText(/this is description/i)).toBeInTheDocument();
       expect(await findByText(/Not completed/i)).toBeInTheDocument();
-      expect(await findByText(/15 May, 2023/i)).toBeInTheDocument();
-      expect(await findByText(/19 May, 2023 14:00/i)).toBeInTheDocument();
+      expect(await findByText(/09 Jun, 2023/i)).toBeInTheDocument();
+      expect(await findByText(/01 Jul, 2023/i)).toBeInTheDocument();
     });
   });
 });
